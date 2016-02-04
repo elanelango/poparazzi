321ca81050854b2365dab2ec62f9b40b08bda28b
Elan <elanelango@gmail.com>
2016-02-04 01:55:44 -0800

    Everything done in video walkthrough

---
+++ app/src/main/java/com/elanelango/poparazzi/Photo.java
@@ -0,0 +1,12 @@
+package com.elanelango.poparazzi;
+
+/**
+ * Created by eelango on 2/3/16.
+ */
+public class Photo {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
---
+++ app/src/main/java/com/elanelango/poparazzi/PhotosAdapter.java
@@ -0,0 +1,40 @@
+package com.elanelango.poparazzi;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by eelango on 2/4/16.
+ */
+public class PhotosAdapter extends ArrayAdapter<Photo> {
+
+    public PhotosAdapter(Context context, List<Photo> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Photo photo = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
+        tvCaption.setText(photo.caption);
+
+        ivPhoto.setImageResource(0);
+
+        Picasso.with(getContext()).load(photo.imageUrl).into(ivPhoto);
+        return convertView;
+    }
+}
